4f990299421f0499c0e3fce01089f303633e9773
Christophe <70860455+cheukhin1024@users.noreply.github.com>
2022-03-27 17:56:26 +0800

    Create Count rainforest satallite image color area.py

---
+++ Count rainforest satallite image color area.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+# load image
+img = cv2.imread(r"C:\Users\Administrator\Downloads\ms image.PNG")
+
+# convert to HSV
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+h,s,v = cv2.split(hsv)
+
+# create mask for green color in hsv
+lower = (36, 25, 25)
+upper = (70, 255,255)
+mask = cv2.inRange(hsv, lower, upper)
+
+# count non-zero pixels in mask
+count=np.count_nonzero(mask)
+print('count:', count)
+
+# save output
+cv2.imwrite('blue_bag_mask.png', mask)
+
+# Display various images to see the steps
+cv2.imshow('mask',mask)
+cv2.waitKey(0)
