3d7e120c024eb1ad2f59de0d00e8a7ce77d135c0
Christophe <70860455+cheukhin1024@users.noreply.github.com>
2022-04-01 14:09:11 +0800

    Update Get data from Delta Lake.py

--- Get data from Delta Lake.py
+++ Get data from Delta Lake.py
@@ -1,4 +1,4 @@
-#After we approve  your application, you can use Azure databricks to view or download data from Delta Lake.
+# After we approve  your application, you can use Azure databricks to view or download data from Delta Lake.
 
 from pyspark import SparkFiles
 from pyspark import SparkContext
@@ -8,8 +8,25 @@ from pyspark.sql import SQLContext
 from pyspark.sql import DataFrame
 from pyspark.sql.types import *
 
-#After displaying the Spark Dataframe, you can press "Download" button.
-display(spark.sql('SELECT * FROM aal_30mins_delta'))
-display(spark.sql('SELECT * FROM aa_30mins_delta'))
+
+# There are 3 Delta Lake formats in our Azure Databricks.
+
+# For S&P500 components data:
+
+# If you want to get current S&P 500 components 1-minute data, please code: "ticker name" + '_1min_delta'. Example of getting Apple (ticker: AAPL) stock data: aapl_1min_delta.
+# If you want to get delisted S&P 500 components 1-minute data, please code: "ticker name" + '_1min_delisted_delta'. Example of getting YAHOO! (ticker: YHOO) stock data: yhoo_1min_delta.
+
+# If you want to get current S&P 500 components 30-minutes data, please code: "ticker name" + '_30mins_delta'. Example of getting Apple (ticker: AAPL) stock data: aapl_30mins_delta.
+# If you want to get delisted S&P 500 components 30-minutes data, please code: "ticker name" + '_30mins_delisted_delta'. Example of getting YAHOO! (ticker: YHOO) stock data: yhoo_30mins_delta.
+
+
+# Example of displaying Apple (ticker: AAPL) 1-minute data
+display(spark.sql('SELECT * FROM aapl_1min_delta'))
+# Example of displaying delisted Yahoo! (ticker: YHOO) 1-minute data
+display(spark.sql('SELECT * FROM yhoo_1min_delta'))
+# Example of displaying Apple (ticker: AAPL) 30-minutes data
 display(spark.sql('SELECT * FROM aapl_30mins_delta'))
-display(spark.sql('SELECT * FROM a_30mins_delta'))
+# Example of displaying delisted YAHOO! (ticker: YHOO) 30-minutes data
+display(spark.sql('SELECT * FROM yhoo_30mins_delta'))
+
+# After displaying the Spark Dataframe, you can press "Download" button.
