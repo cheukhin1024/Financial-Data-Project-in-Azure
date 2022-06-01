1b5333beae84888bbf977115612c2a8315914610
Christophe <70860455+cheukhin1024@users.noreply.github.com>
2022-06-01 08:01:43 +0800

    Update: Check Empty File.py Count satallite image color area.py Financial Data Engineering.py Financial Machine Learning using pyspark ml.py Financial Machine Learning using sklearn.py FirstRate Data Data Engineering.py Get data from Delta Lake.py README.md Useless Code.py [Unusable] First Rate Data Delta Sharing.py count number of file in directory.py

--- FirstRate Data Data Engineering.py
+++ FirstRate Data Data Engineering.py
@@ -13,39 +13,81 @@ from pyspark.sql.types import *
 import json
 
 #LIST, RENAME, AND SAVE ALL FILES AS DELTA LAKE AUTOMATICALLY
-path = '/dbfs/FileStore/tables/FirstRate30mins'
-filename_lists = os.listdir(path)
+path_1min = '/dbfs/FileStore/tables/FirstRate1min'
+filename_lists_1min = os.listdir(path_1min)
 
-df_30mins_ = {}
-_delta ={}
+df_1min_ = {}
+delta_1min ={}
 
-for filename in os.listdir(path):
+for filename_1min in os.listdir(path_1min):
+    
     #split file name
-    name = filename.split('_')[0]
+    name_1min = filename_1min.split('_')[0]
+    
     #create clolumn header names
-    temp = StructType([StructField(name+"_date/time", StringType(), True),StructField(name+"_adjOpen", FloatType(), True),StructField(name+"_adjHigh", FloatType(), True),StructField(name+"_adjLow", FloatType(), True),StructField(name+"_adjClose", FloatType(), True),StructField(name+"_adjVolume", IntegerType(), True)])
+    temp_1min = StructType([StructField(name_1min+"_date/time", StringType(), True),StructField(name_1min+"_adjOpen", FloatType(), True),StructField(name_1min+"_adjHigh", FloatType(), True),StructField(name_1min+"_adjLow", FloatType(), True),StructField(name_1min+"_adjClose", FloatType(), True),StructField(name_1min+"_adjVolume", IntegerType(), True)])
     
     #list and create csv dataframes
-    temp_df = spark.read.format("csv").option("header", "false").schema(temp).load("/FileStore/tables/FirstRate30mins/"+filename)
+    temp_df_1min = spark.read.format("csv").option("header", "false").schema(temp_1min).load("/FileStore/tables/FirstRate1min/"+filename_1min)
     
     #name each dataframes
-    df_30mins_[name] = temp_df
+    df_1min_[name_1min] = temp_df_1min
     
     #name each table
-    table_name = name+'_30mins_delta'
+    table_name_1min = name_1min+'_1min_delta'
     
     #create delta lake for each dataframes
-    df_30mins_[name].write.format("delta").mode("overwrite").saveAsTable(table_name)
+    df_1min_[name_1min].write.format("delta").mode("overwrite").saveAsTable(table_name_1min)
+    
+display(df_1min_['AAL'])
+
+display(spark.sql('SELECT * FROM aal_1min_delta'))
+
+# COMMAND ----------
+
+import os
+import numpy as np
+import pandas as pd
+
+from pyspark import SparkFiles
+from pyspark import SparkContext
+from pyspark.sql import functions
+import pyspark.sql.functions #import avg, col, udf
+from pyspark.sql import SQLContext
+from pyspark.sql import DataFrame
+from pyspark.sql.types import *
+import json
+
+#LIST, RENAME, AND SAVE ALL FILES AS DELTA LAKE AUTOMATICALLY
+path_30mins = '/dbfs/FileStore/tables/FirstRate30mins'
+filename_lists_30mins = os.listdir(path_30mins)
+
+df_30mins_ = {}
+delta_30mins ={}
+
+for filename_30mins in os.listdir(path_30mins):
+    
+    #split file name
+    name_30mins = filename_30mins.split('_')[0]
+    
+    #create clolumn header names
+    temp_30mins = StructType([StructField(name_30mins+"_date/time", StringType(), True),StructField(name_30mins+"_adjOpen", FloatType(), True),StructField(name_30mins+"_adjHigh", FloatType(), True),StructField(name_30mins+"_adjLow", FloatType(), True),StructField(name_30mins+"_adjClose", FloatType(), True),StructField(name_30mins+"_adjVolume", IntegerType(), True)])
+    
+    #list and create csv dataframes
+    temp_df_30mins = spark.read.format("csv").option("header", "false").schema(temp_30mins).load("/FileStore/tables/FirstRate30mins/"+filename_30mins)
+    
+    #name each dataframes
+    df_30mins_[name_30mins] = temp_df_30mins
+    
+    #name each table
+    table_name_30mins = name_30mins+'_30mins_delta'
+    
+    #create delta lake for each dataframes
+    df_30mins_[name_30mins].write.format("delta").mode("overwrite").saveAsTable(table_name_30mins)
     
 display(df_30mins_['AAL'])
-display(df_30mins_['AAPL'])
-display(df_30mins_['AA'])
-display(df_30mins_['A'])
 
 display(spark.sql('SELECT * FROM aal_30mins_delta'))
-display(spark.sql('SELECT * FROM aa_30mins_delta'))
-display(spark.sql('SELECT * FROM aapl_30mins_delta'))
-display(spark.sql('SELECT * FROM a_30mins_delta'))
 
 # COMMAND ----------
 
@@ -100,24 +142,29 @@ display(spark.sql('SELECT * FROM yhoo_30mins_delisted_delta'))
 
 import os
 # check if size of file is 0
+path_remove = "/dbfs/FileStore/tables/FirstRate30mins_Delisted"
 
-for filename_delisted in os.listdir(path_delisted):
-    if os.stat(filename_delisted).st_size == 0:
-        print(f'File {filename_delisted.split("_")[0]} is empty')
-        dbutils.fs.rm("/FileStore/tables/FirstRate30mins_Delisted/" + filename_delisted)
-
-# COMMAND ----------
-
-ABI_30min_spark_DF_newRow = StructType([StructField("ABI_date/time", StringType(), True),StructField("ABI_adjOpen", FloatType(), True),StructField("ABI_adjHigh", FloatType(), True),StructField("ABI_adjLow", FloatType(), True),StructField("ABI_adjClose", FloatType(), True),StructField("ABI_adjVolume", IntegerType(), True)])
-ABI_30min_spark_DF = spark.read.format("csv").option("header", "false").schema(ABI_30min_spark_DF_newRow).load("/FileStore/tables/FirstRate30mins_Delisted/ABI_DELISTED_30min.txt")
-display(ABI_30min_spark_DF)
-
-ABI_30min_spark_DF.write.format("delta").mode("overwrite").saveAsTable("ABI_30mins_delisted_delta")
-display(spark.sql('SELECT * FROM abi_30mins_delisted_delta'))
+for filename_remove in os.listdir(path_remove):
+   #if os.stat(filename_delisted).st_size == 0:
+   #print(f'File {filename_delisted.split("_")[0]} is empty') 
+    dbutils.fs.rm("/FileStore/tables/FirstRate30mins_Delisted" + filename_remove)
 
 # COMMAND ----------
 
-"""
+# MAGIC %fs
+# MAGIC 
+# MAGIC ls /FileStore/tables/FirstRate30mins_Delisted/
+
+# COMMAND ----------
+
+dbutils.fs.rm("/FileStore/tables/FirstRate1min/BBY_1min-1.txt")
+
+
+# COMMAND ----------
+
+display(spark.table('default.a_1min_delta').orderBy('A_date/time'))
+
+# COMMAND ----------
 
 import os
 import numpy as np
@@ -153,5 +200,3 @@ AAPL_30min_koalas_DF.sort_index().head()
 
 AAPL_30min_koalas_DF_new = ks.concat([AAPL_30min_koalas_DF, AAPL_30min_koalas_DF_pct])
 AAPL_30min_koalas_DF_new.head()
-
-"""
