6325afbfa3f21d1f20b976800010e782644d4a6e
Christophe <70860455+cheukhin1024@users.noreply.github.com>
2022-06-19 06:57:55 +0800

    Update Delete Files or Directory in Azure Databricks.py

--- Delete Files or Directory in Azure Databricks.py
+++ Delete Files or Directory in Azure Databricks.py
@@ -1,22 +1,3 @@
-# Databricks notebook source
-
-
-import os
-# check if size of file is 0
-path_remove = "/dbfs/user/hive/warehouse"
-
-for filename_remove in os.listdir(path_remove):
-   #Delete file if the file is empty
-    if os.stat(filename_delisted).st_size == 0:
-   #print(f'File {filename_delisted.split("_")[0]} is empty') 
-    dbutils.fs.rm("/dbfs/user/hive/warehouse/" + filename_remove)
-    
-    
- # COMMAND ----------   
-    
-    
-    
-    
 #Delete directory    
 dbutils.fs.rm("/tmp", True)
     
