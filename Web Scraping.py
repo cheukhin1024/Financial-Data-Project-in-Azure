16381de761c6d5496c11ef9347027fcfcb10c064
Christophe <70860455+cheukhin1024@users.noreply.github.com>
2023-08-31 11:41:45 +0800

    Update: Bloomberg Speech to Text.py Check Empty File.py Count number of file in directory.py Count satallite image color area.py Delete Files or Directory.py Extract FirstRate Stock Tickers Issue Date & Delisted Date.py Financial Data Engineering.py FirstRate 1min data ETL.py FirstRate 30min data ETL.py Get NASA OCO-2 satallite images data.py Get NASA OCO-2 satallite images data.sh Hierarchical Risk Parity Algorithm.py K-Mean clustering  in Sector Classification using sklearn.py K-Mean clustering in Sector Classification using SparkML.py List Tables Pair Trading.py README.md Stream IEX Market Data.py User Defined Functions.py VACUUM FirstRate 30min delta tables.py Web Scraping.py

--- Web Scraping.py
+++ Web Scraping.py
@@ -1,1 +1,59 @@
+# Databricks notebook source
+# DBTITLE 1,Web Scraping from Hindenburg Research
+from bs4 import BeautifulSoup 
+import requests
 
+url = "https://hindenburgresearch.com/page/{}/"
+page_num = 1
+page = requests.get(url.format(page_num))
+
+# COMMAND ----------
+
+soup = BeautifulSoup(page.text, 'html')
+print(soup.prettify())
+
+# COMMAND ----------
+
+headline = soup.find('h1').text
+print(headline)
+print(type(headline))
+
+# COMMAND ----------
+
+titles_arr = []
+
+next_text = soup.find("a", class_="next page-numbers")
+
+while next_text is not None:
+    #titles = soup.find_all(['h2','span'], class_ = ['post-title','entry-title-primary'])
+    titles = soup.find_all(['h2'], class_ = ['post-title'])
+    for title in titles:
+        print(title.text)
+        titles_arr.append(title.text)
+    page_num+=1
+    page = requests.get(url.format(page_num))
+    soup = BeautifulSoup(page.text, 'html')
+    next_text = soup.find("a", class_="next page-numbers")
+
+# COMMAND ----------
+
+#Last page that does not have next button
+if next_text is None:
+    titles = soup.find_all(['h2'], class_ = ['post-title'])
+    for title in titles:
+        print(title.text)
+        titles_arr.append(title.text)
+
+# COMMAND ----------
+
+print(page_num)
+print(url.format(page_num))
+
+# COMMAND ----------
+
+print(len(titles_arr))
+
+# COMMAND ----------
+
+for title_text in titles_arr:
+    print(title_text)
